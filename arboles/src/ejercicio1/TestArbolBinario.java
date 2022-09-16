21bd57f5eef0e9858a8aabc3cff5756926ff1efd
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-09-16 18:06:41 -0300

    :sparkles: [ADD] Ejercicio2 Arboles

---
+++ arboles/src/ejercicio1/TestArbolBinario.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+public class TestArbolBinario {
+ public static void main(String[] args) {
+	 ArbolBinario<Integer> ab = new ArbolBinario<Integer>(2);
+	 ab.agregarHijoIzquierdo(new ArbolBinario<Integer>(2));
+	 ab.agregarHijoDerecho(new ArbolBinario<Integer>(3));
+	 ab.getHijoDerecho().agregarHijoIzquierdo(new ArbolBinario<Integer>(10));
+	 ab.getHijoDerecho().agregarHijoDerecho(new ArbolBinario<Integer>(20));
+	 System.out.println(ab.contarHojas());
+ }
+}
