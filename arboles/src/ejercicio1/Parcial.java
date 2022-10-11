faf52ab85e45ddc33f362e33b707f52f81ad8b35
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-10-11 20:26:27 -0300

    :memo: [UPDATE] ejercicio parcial

--- arboles/src/ejercicio1/Parcial.java
+++ arboles/src/ejercicio1/Parcial.java
@@ -1,25 +1,56 @@
 package ejercicio1;
 
-public class Parcial {
+import tp04.ejercicio3.ArbolGeneral;
 
-	public boolean esDeSeleccion(ArbolGeneral<Integer> arbol) {
-		boolean flag = false;
+public class Parcial {
+    public static void main(String[] args) {
+       ArbolGeneral<Integer> arbol = null;
+       ListaGenerica<ArbolGeneral<Integer>> hijos = new ListaEnlazadaGenerica<ArbolGeneral<Integer>>();
+       hijos.agregarFinal(new ArbolGeneral<Integer>(10));
+       hijos.agregarFinal(new ArbolGeneral<Integer>(7));
+       hijos.agregarFinal(new ArbolGeneral<Integer>(9));
+       arbol = new ArbolGeneral<Integer>(7, hijos);
+       
+       if (esDeSeleccion(arbol)) {
+    	   System.out.println("Es de seleccion");
+       }else System.out.println("No es de seleccion");
+    }
+    
+	public static boolean esDeSeleccion(ArbolGeneral<Integer> arbol) {
+		int min = Integer.MAX_VALUE;
+		boolean flag = true;
 		if (!arbol.esVacio()) {
-			esDeSeleccionRecu(arbol, flag);
-		}
+			ColaGenerica<ArbolGeneral<Integer>> cola = new ColaGenerica<ArbolGeneral<Integer>>();
+			ArbolGeneral<Integer> arbolAux = null;
+			cola.encolar(arbol);
+			cola.encolar(null);
+			while(!cola.esVacia()) {
+				arbolAux = cola.desencolar();
+				if (arbolAux != null) {
+					ListaGenerica<ArbolGeneral<Integer>> hijos = arbolAux.getHijos();
+					hijos.comenzar();
+					while (!hijos.fin()) {
+						ArbolGeneral<Integer> aux = hijos.proximo();
+						if (aux.getDato() < min) {
+							min = aux.getDato();
+						}
+						cola.encolar(aux);
+					}
+					
+					if (arbolAux.getDato() != min) {
+						flag= false;
+						break;
+					}
+				}else {
+					if (!cola.esVacia()) {
+						cola.encolar(null);
+					}
+				}
+			}
+ 		}
 		return flag;
 	}
 	
-	public boolean esDeSeleccionRecu(ArbolGeneral<Integer> ab, boolean flag) {
-		if (ab.esHoja()) return flag;
-		int dato = ab.getDato();
-		ListaGenerica<ArbolGeneral<Integer>> hijos = ab.getHijos();
-		hijos.comenzar();
-		while(!hijos.fin() && !flag) {
-			ArbolGeneral<Integer> aux = hijos.proximo(); 
-			if (!(dato <= aux.getDato())) flag = true;
-			else flag = esDeSeleccionRecu(aux, flag);
-		}
-		return flag;
-	}
+	
+
 }
